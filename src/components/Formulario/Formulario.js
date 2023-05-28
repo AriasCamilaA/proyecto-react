5e875017a6d952c96fe4ace0f154d182a60e0047
AriasCamilaA <ariasruizcamilaa@gmail.com>
2023-05-27 21:29:46 -0500

    Completando Formulario

--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -1,18 +1,56 @@
+import { useState } from "react";
 import "./Formulario.css";
 import CampoTexto from "../CampoTexto";
 import ListaOpciones from "../ListaOpciones";
 import Boton from "../Boton";
 
 const Formulario = () => {
+
+    const [nombre, setNombre] = useState("")
+    const [puesto, setPuesto] = useState("")
+    const [foto, setFoto] = useState("")
+    const [equipo, setEquipo] = useState("")
+
+    const manejarEnvio = (e) =>{
+        e.preventDefault()
+        let datosAEnviar = {
+            nombre,
+            puesto,
+            foto,
+            equipo
+        }
+        console.log(datosAEnviar)
+    }
     return <section className="formulario">
-        <form>
+        <form onSubmit={manejarEnvio}>
             <h2>
                 Rellena el formulario para crear el colaborador.
             </h2>
-            <CampoTexto titulo="Nombre" placeholder="ingresa nombre"/>
-            <CampoTexto titulo="Puesto" placeholder="ingresa puesto"/>
-            <CampoTexto titulo="Foto" placeholder="ingresa enlace de foto"/>
-            <ListaOpciones/>
+            <CampoTexto 
+                titulo="Nombre" 
+                placeholder="ingresa nombre" 
+                required 
+                valor={nombre} 
+                actualizarValor={setNombre}
+                />
+            <CampoTexto 
+                titulo="Puesto" 
+                placeholder="ingresa puesto" 
+                required
+                valor={puesto} 
+                actualizarValor={setPuesto}
+                />
+            <CampoTexto 
+                titulo="Foto" 
+                placeholder="ingresa enlace de foto" 
+                required
+                valor={foto} 
+                actualizarValor={setFoto}
+            />
+            <ListaOpciones
+                valor={equipo}
+                actualizarValor={setEquipo}
+            />
             <Boton>Crear</Boton>
         </form>
     </section>
--- src/components/ListaOpciones/index.js
+++ src/components/ListaOpciones/index.js
@@ -1,6 +1,6 @@
 import "./ListaOpciones.css";
 
-const ListaOpciones = () =>{
+const ListaOpciones = (props) =>{
 
     const equipos = [
         "Programación",
@@ -12,9 +12,14 @@ const ListaOpciones = () =>{
         "Innovación y Gestión"
     ]
 
+    const manejarCambio = (e) => {
+        props.actualizarValor(e.target.value)    
+    }
+
     return <div className="lista-opciones">
         <label>Equipos</label>
-        <select>
+        <select value={props.valor} onChange={manejarCambio}>
+            <option value="" disabled defaultValue="" hidden>Seleccionar equipo</option>
             {equipos.map((equipo, index) => <option key={index}>{equipo}</option>)}
         </select>
     </div>
